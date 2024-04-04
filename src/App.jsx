e2fb6b560336a599a0f55175ef0968cfa4094160
Utku Ulu <utku.ulu@bilgeadamboost.com>
2024-04-04 18:05:04 +0300

    Refactor API key in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom"; // Import BrowserRouter, Routes, and Route components
+import { Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import TitleCardsContainer from "./Components/TitleCardsContainer/TitleCardsContainer";
-import MovieDetail from "./Components/MovieDetail"; // Import MovieDetail component
+import MovieDetail from "./Components/MovieDetail";
+
+// Define constant for API key
+const API_KEY = "5b9c3630ad1996e84b1b8d95882bc995";
 
 function App() {
   return (
@@ -17,17 +20,17 @@ function App() {
               <TitleCardsContainer
                 key="recommendations"
                 titleText="Tam Davşanlık Öneriler"
-                apiKey="https://api.themoviedb.org/3/discover/movie?api_key=5b9c3630ad1996e84b1b8d95882bc995&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1"
+                apiKey={`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1`}
               />
               <TitleCardsContainer
                 key="romantic"
-                apiKey="https://api.themoviedb.org/3/discover/movie?api_key=5b9c3630ad1996e84b1b8d95882bc995&language=en-US&with_genres=10749&sort_by=popularity.desc&include_adult=true&include_video=false&page=1"
+                apiKey={`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10749&sort_by=popularity.desc&include_adult=true&include_video=false&page=1`}
                 titleText="Romantik Filmler"
               />
               <TitleCardsContainer
                 key="musical"
                 titleText="Muzikal Davşan Filmleri"
-                apiKey="https://api.themoviedb.org/3/discover/movie?api_key=5b9c3630ad1996e84b1b8d95882bc995&language=en-US&with_genres=10402&sort_by=popularity.desc&include_adult=true&include_video=false&page=1"
+                apiKey={`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10402&sort_by=popularity.desc&include_adult=true&include_video=false&page=1`}
               />
             </div>
           }

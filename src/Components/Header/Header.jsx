0f71e8e8965dff8f5c1fc4174115f8d709898114
Utku Ulu <utku.ulu@bilgeadamboost.com>
2024-04-10 22:54:20 +0300

    Add click outside menu handling in Header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const headerRef = useRef(null);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (headerRef.current && !headerRef.current.contains(event.target)) {
+        setMenuOpen(false);
+      }
+    };
+
+    window.addEventListener("click", handleClickOutside);
+
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="header-main">
-      <div className="header">
+      <div className="header" ref={headerRef}>
         <div className="logo-container">
           <Link to="/" className="logo-link">
             <p className="logo-item">🐰 RABBITFLIX</p>
